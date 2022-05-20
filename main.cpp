32c0b5ee3abfdf76bd0785999a32acaf5e300c04
Your Name <efimovaYAn@mpei.ru>
2022-05-20 14:15:43 +0400

    code: добавлена функция подсчета процента

--- main.cpp
+++ main.cpp
@@ -1,193 +1,115 @@
-
 #include <iostream>
 #include <vector>
-
+#include <math.h>
+#include "histogram.h"
+#include "svg.h"
 using namespace std;
 
-vector<double> input_numbers(size_t count)
+
+vector <double> input_numbers ( size_t count)
 {
     vector<double> result(count);
-    for(size_t i = 0; i < count; i++)
+    for (size_t i = 0; i < count; i++)
     {
         cin >> result[i];
     }
     return result;
 }
 
-void find_minmax(const vector<double>& numbers, double& min, double& max)
-{
-    min = max = numbers[0];
-    for(double number : numbers)
-    {
-        if (min > number)
-        {
-            min = number;
-        }
-        if (max < number)
-        {
-            max = number;
-        }
-    }
-}
-vector<size_t> make_histogramm(const vector<double>& numbers, size_t bin_count)
+
+
+vector <size_t> make_histogram( const vector<double> numbers, size_t bin_count)
 {
     double min, max;
-    find_minmax(numbers, min, max);
-
-    vector<size_t> bins(bin_count, 0);
-    double bin_size = (max - min) / bin_count;
-    for (size_t i = 0; i < numbers.size(); i++)
+    find_minmax( numbers, min, max) ;
+    vector<size_t> bins(bin_count,0); //переменная показывающая количество чисел в заданном диапазоне
+    double bin_size =( max-min)/ bin_count; //разме корзины
+    for(size_t i=0; i < numbers.size(); i++)
     {
-        bool found = false;
-        for(size_t j = 0; j < (bin_count - 1) && !found; j++)
+        bool found=false;
+        for ( size_t j = 0; j <( bin_count-1) && !found; j++)
         {
-            auto lo = min + j * bin_size;
-            auto hi = min + (j + 1) * bin_size;
-            if (lo <= numbers[i] && numbers[i] < hi)
+            auto lo = min + j * bin_size; // нижняя граница корзины
+            auto hi = min + (j+1) * bin_size; // верхняя граница корзины
+            if((lo <= numbers[i]) && (numbers[i] < hi))
             {
                 bins[j]++;
                 found = true;
             }
         }
-        if (!found)
+        if (!found)  //для максимального числа
         {
-            bins[bin_count - 1]++;
+            bins [bin_count-1]++;
+
         }
     }
     return bins;
 }
 
-void show_histogramm_text(const vector<size_t>& bins)
+show_histogram_text(const vector <size_t>& bins)
 {
-    size_t SCREEN_WIDTH = 80;
-    const size_t max_asterix = SCREEN_WIDTH - 3 - 1;
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH -3-1;
 
-    size_t max_bin = bins[0];
+    size_t max_bin = bins[0]; //максимальная высота столбца
     for (size_t bin: bins)
     {
-        if (max_bin < bin)
+        if(max_bin < bin)
         {
             max_bin = bin;
         }
     }
-
     for(size_t bin: bins)
     {
         size_t height = bin;
-
-        //Проверка, нужно ли масштабировать столбцы
-        if (max_bin > max_asterix)
+        //проверить, нужно ли масштабировать данные, если нужно, пересчитать height
+        if (max_bin > MAX_ASTERISK)
         {
-            height = max_asterix * (static_cast<double>(bin) / max_bin);
+            height = MAX_ASTERISK*(static_cast<double> (bin) / max_bin); //коэфициент
         }
-
-        if (bin < 100)
+        size_t procent= bin;
+        if (procent < 100)
         {
-            cout << ' ';
+            cout << " ";
         }
-        if (bin < 10)
+        if (procent < 10)
         {
-            cout << ' ';
+            cout<< " ";
         }
-        cout << bin << "|";
-        for (size_t i = 0; i < height; i++)
+        cout << procent << "|";
+        for (size_t i=0; i < height; i++)
         {
-            cout << "*";
+            cout<< "*";
         }
         cout << endl;
     }
 }
 
-void svg_begin(double width, double height)
-{
-    cout << "<?xml version = '1.0' encoding='UTF-8'?>\n";
-    cout << "<svg ";
-    cout << "width='" << width << "' ";
-    cout << "height='" << height << "' ";
-    cout << "viewbox='0 0 " << width << " " << height << "' ";
-    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
 
-void svg_text(double left, double baseline, string text)
-{
-    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>";
-}
-
-void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black")
-{
-    //<rect x='0' y='0' width='100' height='200' />
-    cout << "<rect x='" << x << "' y='" << y
-        << "' width='" << width << "' height='" << height
-        << "' stroke='" << stroke << "' fill='" << fill << "'/>\n";
-}
-
-void svg_end()
-{
-    cout << "</svg>\n";
-}
-
-void show_histogramm_svg(const vector<size_t>& bins)
-{
-    const auto IMAGE_WIDTH = 400;
-    const auto IMAGE_HEIGHT = 300;
-    const auto TEXT_LEFT = 20;
-    const auto TEXT_BASELINE = 20;
-    const auto TEXT_WIDTH = 50;
-    const auto BIN_HEIGHT = 30;
-    const auto BLOCK_WIDTH = 10;
-    const auto RECT_STROKE = "red";
-    const auto RECT_FILL = "#ffcccc";
-
-    size_t max_bin = bins[0];
-    for (size_t bin : bins)
-    {
-        if(bin > max_bin)
-        {
-            max_bin = bin;
-        }
-    }
-    auto max_bin_width = BLOCK_WIDTH * max_bin;
-    const auto MAX_BIN_WIDTH = IMAGE_WIDTH - TEXT_WIDTH;
-
-    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-
-    double top = 0;
-    for(size_t bin : bins)
-    {
-        double bin_width = BLOCK_WIDTH * bin;
-        if(MAX_BIN_WIDTH < max_bin_width)
-        {
-            bin_width = static_cast<double>(bin_width) / max_bin_width * MAX_BIN_WIDTH;
-        }
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, RECT_STROKE, RECT_FILL);
-        top += BIN_HEIGHT;
-    }
-
-    svg_end();
-}
 
 int main()
 {
-    //Ввод данных
+    //ввод данных
     size_t number_count;
-    cerr << "Enter number count: ";
-    cin >> number_count;
+    cerr<< "Enter number count:";
+    cin>> number_count;
 
-
-    cerr << "Enter numbers: ";
     const auto numbers = input_numbers(number_count);
 
     size_t bin_count;
-    cerr << "Enter bin count: ";
-    cin >> bin_count;
+    cerr<< "Enter bin count:" ;
+    cin>> bin_count;
 
 
-    //Расчет гистограммы
-    const auto bins = make_histogramm(numbers, bin_count);
+    //рассчет гистограммы
+    const auto bins= make_histogram(numbers, bin_count);
 
-    //Вывод гистограммы
-    show_histogramm_svg(bins);
+	const auto proc = procent (number_count, bins) ;
+    //вывод гистограммы
+    show_histogram_svg (bins, proc);
+
 
     return 0;
 }
+
+
