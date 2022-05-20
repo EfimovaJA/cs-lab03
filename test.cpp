70af6d2d2e1fe2f582748f849ab36f14c9d8cb49
Your Name <efimovaYAn@mpei.ru>
2022-05-20 14:19:17 +0400

    code: добавлены тестовые примеры

--- test.cpp
+++ test.cpp
@@ -36,7 +36,54 @@ void test_one()
     assert(max == 1);
 }
 
+void test_null()
+{
+    double min = 0;
+    double max = 0;
+    find_minmax({}, min, max);
+    assert(min == 0);
+    assert(max == 0);
+}
 
+
+
+void test_numbers()
+{
+    vector <size_t> data = {10, 30, 60};
+	vector <size_t> input_data = { 1, 3, 6 };
+    vector <size_t> arr = procent(10,{input_data });
+    assert(data == arr);
+    return;
+}
+
+void test_numbers2()
+{
+    vector <size_t> data = {100};
+    vector <size_t> input_data = { 1 };
+
+    vector <size_t> arr = procent (1, {input_data});
+    assert(data == arr);
+    return;
+}
+void test_numbers3()
+{
+	vector <size_t> data = {0};
+    vector <size_t> input_data = { 0 };
+
+    vector <size_t> arr = procent (1, {input_data});
+    assert(data == arr);
+    return;
+}
+
+void test_numbers4()
+{
+	vector <size_t> data = { };
+    vector <size_t> input_data = { };
+
+    vector <size_t> arr = procent (0, {input_data});
+    assert(data == arr);
+    return;
+}
 
 int main()
 {
@@ -44,6 +91,11 @@ int main()
     test_negative();
     test_simular();
     test_one();
-
+    test_null();
+	test_numbers();
+	test_numbers2();
+	test_numbers3() ;
+	test_numbers4 () ;
 }
+
 
