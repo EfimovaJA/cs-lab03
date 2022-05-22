67fb95661bf1ec99819a41756cb39aab88f06279
Your Name <efimovaYAn@mpei.ru>
2022-05-22 23:21:02 +0400

    добавлены структуры

--- histogram.cpp
+++ histogram.cpp
@@ -1,17 +1,18 @@
 #include "histogram.h"
 
-void find_minmax(const vector<double> numbers, double& min, double& max)
+using namespace std;
+
+void find_minmax(const vector<double>& numbers, double& min, double& max)
 {
     if(numbers.size() == 0)
     {
         return;
     }
-    else {
-	min = numbers[0];
-    max = numbers[0];
-    for(double number: numbers)
+
+    min = max = numbers[0];
+    for(double number : numbers)
     {
-        if(min > number)
+        if (min > number)
         {
             min = number;
         }
@@ -20,15 +21,77 @@ void find_minmax(const vector<double> numbers, double& min, double& max)
             max = number;
         }
     }
-   } // (здесь код поиска минимума и максимума)
 }
 
-vector <size_t> procent (size_t number_count, const vector <size_t>& bins)
+vector<size_t> make_histogramm(Input data)
 {
-    vector <size_t> x (bins.size());
-    for ( size_t i=0; i< bins.size(); i++)
+    const vector<double>& numbers = data.numbers;
+    size_t& bin_count = data.bin_count;
+
+    double min, max;
+    find_minmax(numbers, min, max);
+
+    vector<size_t> bins(bin_count, 0);
+    double bin_size = (max - min) / bin_count;
+    for (size_t i = 0; i < numbers.size(); i++)
     {
-        x[i] = static_cast<double> (bins[i]) / number_count * 100;
+        bool found = false;
+        for(size_t j = 0; j < (bin_count - 1) && !found; j++)
+        {
+            auto lo = min + j * bin_size;
+            auto hi = min + (j + 1) * bin_size;
+            if (lo <= numbers[i] && numbers[i] < hi)
+            {
+                bins[j]++;
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            bins[bin_count - 1]++;
+        }
     }
-    return x;
+    return bins;
+}
+
+
+void show_histogramm_text(const vector<size_t>& bins)
+{
+    size_t SCREEN_WIDTH = 80;
+    const size_t max_asterix = SCREEN_WIDTH - 3 - 1;
+
+    size_t max_bin = bins[0];
+    for (size_t bin: bins)
+    {
+        if (max_bin < bin)
+        {
+            max_bin = bin;
+        }
+    }
+
+    for(size_t bin: bins)
+    {
+        size_t height = bin;
+
+        //Проверка, нужно ли масштабировать столбцы
+        if (max_bin > max_asterix)
+        {
+            height = max_asterix * (static_cast<double>(bin) / max_bin);
+        }
+
+        if (bin < 100)
+        {
+            cout << ' ';
+        }
+        if (bin < 10)
+        {
+            cout << ' ';
+        }
+        cout << bin << "|";
+        for (size_t i = 0; i < height; i++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
 }
